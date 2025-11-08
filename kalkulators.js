428be0d72f8c4e4686ad8c49913262b003131779
pipisa <your@email.ex>
2025-11-08 13:14:29 +0200

    tiek salabotas dažas kļudas, tiek pieaddots select drop downs ar reizinasanu un dalisanu

--- kalkulators.js
+++ kalkulators.js
@@ -1,4 +1,13 @@
 const inputId = document.getElementById("inputId");
 const ButtonId = document.getElementById("ButtonId");
 const OutputText = document.getElementById("OutputText");
+const SelectId = document.getElementById("SelectId");
+const ReizinātId = document.getElementById("ReizinātId");
+const DalītID = document.getElementById("DalītID");
 
+
+
+inputId.addEventListener('click', HandleClick);
+function HandleClick() {
+
+}
