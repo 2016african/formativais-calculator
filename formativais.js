5f4adb323d3d36e632f6a3590cfacece33e5f60c
pipisa <your@email.ex>
2025-11-08 12:55:35 +0200

    const PI uzrakstīts, GetElementByID ir ari izmantots 3 elementiem

--- formativais.js
+++ formativais.js
@@ -1,3 +1,6 @@
 const PI = 3.14;
 
 
+const RadiusBox = document.getElementById("RadiusBox");
+const IesniegtButton = document.getElementById("IesniegtButton");
+const output = document.getElementById("output");
