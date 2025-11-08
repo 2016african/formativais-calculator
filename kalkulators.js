9264b5ef8d81b8677c91bead38c3d05c77908e0e
pipisa <your@email.ex>
2025-11-08 13:36:36 +0200

    netika pabeigts darbs lidz galam, programma tikai reizina, nemaceju saprast ka var arī dalīt

--- kalkulators.js
+++ kalkulators.js
@@ -1,4 +1,5 @@
-const inputId = document.getElementById("inputId");
+const numurs1ID = document.getElementById("numurs1ID");
+const numurs2ID = document.getElementById("numurs2ID");
 const ButtonId = document.getElementById("ButtonId");
 const OutputText = document.getElementById("OutputText");
 const SelectId = document.getElementById("SelectId");
@@ -6,8 +7,21 @@ const ReizinātId = document.getElementById("ReizinātId");
 const DalītID = document.getElementById("DalītID");
 
 
+let rezultats = 0;
 
-inputId.addEventListener('click', HandleClick);
+ButtonId.addEventListener('click', HandleClick);
 function HandleClick() {
+
+     x = numurs1ID.value;
+     z = numurs2ID.value;
+
+    if ( ReizinātId ) {
+    rezultats = x * z
+    }
+else {
+ rezultats = x / z
+}
+
+OutputText.innerText = "Rezultāts ir " + rezultats;
 
 }
