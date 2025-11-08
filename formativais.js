5f5817ca9686acac3e459c8022bbd12e401be04a
pipisa <your@email.ex>
2025-11-08 13:03:22 +0200

    Pabeigts pirmais uzdevums, funkcija HandleClick ir pieaddota un ievadot numuru izvada rinka linijas un ari laukuma rezulatutu

--- formativais.js
+++ formativais.js
@@ -4,3 +4,14 @@ const PI = 3.14;
 const RadiusBox = document.getElementById("RadiusBox");
 const IesniegtButton = document.getElementById("IesniegtButton");
 const output = document.getElementById("output");
+
+IesniegtButton.addEventListener('click', HandleClick) ;
+function HandleClick() {
+
+let r = RadiusBox.value;
+let laukums = PI * r * r;
+let garums = 2 * PI * r;
+
+output.innerText =
+"Laukums ir : " + laukums + "\n" + "Garums ir : " + garums;
+}
